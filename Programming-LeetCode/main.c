7015e7e0dd50dddb5de50beed1ace2350107d30b
Sukhwant <prafullitsukhwant@gmail.com>
2017-08-02 11:36:01 +0530

    Initial Commit

---
+++ Programming-LeetCode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Programming-LeetCode
+//
+//  Created by Prafullit, Sukhwant on 02/08/17.
+//  Copyright © 2017 Prafullit, Sukhwant. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
